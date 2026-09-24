887de130cd09180e1010184ba143a3439ff6075e
deepak640 <ayushdeepnegi@gmail.com>
2026-09-24 18:16:50 +0530

    Revert "fix(isolation): strictly isolate centre teams and completions by physical location"
    This reverts commit 93f2af278c699f8383f3721bdbb948d87982eeb5.

--- src/services/reportService.js
+++ src/services/reportService.js
@@ -91,7 +91,7 @@ async function generateAndSendDailyReport(targetDate = todayKey(), customRecipie
     }
 
     const groupStats = Object.entries(groups).map(([loc, emps]) => {
-      const team = emps.length > 0 ? emps : [cm];
+      const team = Array.from(new Set([cm, ...emps]));
       const doneTaskIds = new Set();
       team.forEach((emp) => {
         Object.keys(compMap[emp.id] || {}).forEach((t) => doneTaskIds.add(t));
@@ -102,9 +102,10 @@ async function generateAndSendDailyReport(targetDate = todayKey(), customRecipie
       return { loc, emps: emps.length > 0 ? emps.map((e) => e.name).join(", ") : cm.name, done, total, pct };
     });
 
-    const totalGroupPct = groupStats.reduce((sum, g) => sum + g.pct, 0);
-    const avgScore = groupStats.length > 0
-      ? Math.round(totalGroupPct / groupStats.length)
+    const totalDone = groupStats.reduce((sum, g) => sum + g.done, 0);
+    const totalTasks = groupStats.reduce((sum, g) => sum + g.total, 0);
+    const avgScore = totalTasks > 0
+      ? Math.round((totalDone / totalTasks) * 100)
       : 0;
 
     return {
